ec46902677b44e78b4edd32e8a2060aa8ad293ca
YussefAbdulmajeed <140410089+YussefAbdulmajeed@users.noreply.github.com>
2024-12-05 18:11:08 +0200

    youssef Updated gui.fsx.
    I added a function to take the file path or text.

---
+++ gui.fsx
@@ -0,0 +1,28 @@
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+// Function to read text from a file
+let loadTextFromFile (filePath: string) =
+    try
+        File.ReadAllText(filePath)
+    with
+    | ex -> sprintf "Error reading file: %s" ex.Message
+
+let getUserInput () =
+    printfn "Choose input method: 1 - Direct Text, 2 - upload File using path"
+    match Console.ReadLine() with
+    | "1" -> 
+        printfn "Enter your text:"
+        Console.ReadLine()
+    | "2" -> 
+        printfn "Enter the file path:"
+        let filePath = Console.ReadLine()
+        if File.Exists filePath then
+            loadTextFromFile(filePath)
+        else
+            printfn "File not found. Exiting."
+            ""
+    | _ -> 
+        printfn "Invalid choice. Exiting."
+        ""
