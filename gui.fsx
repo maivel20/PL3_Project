160f26a82339815dd56e73f7ed67380652a61588
maivel20 <maivelwageh@gmail.com>
2024-12-07 14:35:23 +0200

    Update gui.fsx

--- gui.fsx
+++ gui.fsx
@@ -30,3 +30,6 @@ let cleanText (text: string) =
    // Keep "F#" as a special case and remove other punctuation
    let cleaned = Regex.Replace(text, @"[^\w\s#]", "")
    cleaned
+
+let countWords (text: string) =
+    text.Split([|' '; '\n'; '\t'; '\r'|], StringSplitOptions.RemoveEmptyEntries).Length
